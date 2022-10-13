264e24456ef344f06f3b1785f71665cb142acd24
Naman Jain <namanjain15032001@gmail.com>
2022-10-13 20:08:17 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class extends Component {
+    render() {
+        return (
+            <div style={{ display: 'flex', padding: '0.5' }}>
+                <h1>Movies App</h1>
+                <h2 style={{ marginLeft: '2rem', marginTop: '1.5rem' }}>Favorites</h2>
+
+            </div>
+        )
+    }
+}
